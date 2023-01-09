d5c64a7472141972eeebb72f91d039b2a303fa69
WoolenLamb2171 <85489253+WoolenLamb2171@users.noreply.github.com>
2023-01-09 22:54:22 +0300

    Update Input.js

--- src/Input.js
+++ src/Input.js
@@ -1,6 +1,11 @@
 import React from "react";
+import clsx from "clsx";
 
 export default function Input(props) {
-  const { children, type = "text", ...rest } = props;
-  return <input className="ui-textfield" type={type} {...rest} />;
+  const { className, children, type, ...rest } = props;
+  const classes = clsx("ui-textfield", className);
+  const inputType = clsx({ text: !props.type }, type);
+  console.log(inputType);
+  return <input className={classes} type={inputType} {...rest} />;
 }
+
