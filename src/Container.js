f18a4830f532af4dc260f42cb46ea368a2159b9e
WoolenLamb2171 <85489253+WoolenLamb2171@users.noreply.github.com>
2023-01-09 22:56:07 +0300

    Update Container.js

--- src/Container.js
+++ src/Container.js
@@ -1,9 +1,11 @@
 import React from "react";
+import clsx from "clsx";
 
 export default function Container(props) {
-  const { children, ...rest } = props;
+  const { children, className, ...rest } = props;
+  const classes = clsx("ui-container", className);
   return (
-    <div className="ui-container" {...rest}>
+    <div className={classes} {...rest}>
       {children}
     </div>
   );
