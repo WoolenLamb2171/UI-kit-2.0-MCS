b5a6bd1a282ba5e4e61562d69e89616da6bdd667
WoolenLamb2171 <85489253+WoolenLamb2171@users.noreply.github.com>
2023-01-09 22:56:47 +0300

    Update Link.js

--- src/Link.js
+++ src/Link.js
@@ -1,9 +1,11 @@
 import React from "react";
+import clsx from "clsx";
 
 export default function Link(props) {
-  const { children, ...rest } = props;
+  const { children, className, ...rest } = props;
+  const classes = clsx("ui-link", className);
   return (
-    <a {...rest} className="ui-link">
+    <a {...rest} className={classes}>
       {children}
     </a>
   );
